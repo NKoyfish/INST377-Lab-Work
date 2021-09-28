00cb960fd46b5d3c2ee8f9c0d8d0c0b3e44ddd57
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2021-09-28 13:08:39 -0400

    doodle jump initial start

--- client/doodle/app.js
+++ client/doodle/app.js
@@ -1,18 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid');
-  const doodler = document.createdElement('div');
+/* eslint-disable prefer-const */
+/* eslint-disable no-plusplus */
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const doodler = document.createdElement("div");
   const doodlerLeftSpace = 50;
   const doodlerBottomSpace = 150;
+  let platformCount = 5;
   function createDoodler() {
     grid.appendChild(doodler);
-    doodler.classList.add('doodler');
+    doodler.classList.add("doodler");
     doodler.style.left = `${doodlerLeftSpace}px`;
     doodler.style.bottom = `${doodlerBottomSpace}px`;
   }
+  class Platform {
+    constructor(newPlatformBottom) {
+      this.bottom = newPlatformBottom;
+      this.left = Math.random() * 315;
+      this.visual = document.createElement("div");
 
+      const visual = this.visual;
+      visual.classList.add("platform");
+      visual.style.left = this.left + "px";
+      visual.style.bottom = this.bottom + "px";
+    }
+  }
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+      let platformGap = 600 / platformCount;
+      let newPlatformBottom = 100 + i * platformGap;
+      let newPlatform = new platformGap();
+    }
+  }
   function start() {
     if (!isGameOver) {
       createDoodler();
+      createPlatforms();
     }
   }
   start();
