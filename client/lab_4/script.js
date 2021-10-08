ed9ab2aed49369e38a3b288ed4ead5eb1cf3357f
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2021-10-08 17:21:30 -0400

    Lab 4 and 6
    lab 4 now changes styles on click
    lab 6 minor css

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,19 +2,29 @@ let slidePosition = 0;
 const slides = document.querySelectorAll('.carousel__item');
 const totalSlides = slides.length;
 
-document.
-querySelector('#carousel__button--next')
-  .addEventListener("click", function() {
-    moveToNextSlide();
-  });
-document.
-querySelector('#carousel__button--prev')
-  .addEventListener("click", function() {
-    moveToPrevSlide();
-  });
+next = document.querySelector('.carousel__button--next');
+next.addEventListener('click', () => {
+  moveToNextSlide();
+  const pclasses = prev.classList
+  const nclasses = next.classList
+  newClass = 'carousel__button--next lastClick'
+  if (!nclasses.contains('lastClick')) {next.classList = newClass}
+  if (pclasses.contains('lastClick')) {prev.classList = 'carousel__button--prev '}
+});
+prev = document.querySelector('.carousel__button--prev');
+prev.addEventListener('click', () => {
+  moveToPrevSlide();
+  const pclasses = prev.classList
+  const nclasses = next.classList
+  newClass = 'carousel__button--prev lastClick'
+  if (!pclasses.contains('lastClick')) {prev.classList = newClass}
+  if (nclasses.contains('lastClick')) {next.classList = 'carousel__button--next '}
+  console.log(newClass)
+  prev.classList = newClass
+});
 
 function updateSlidePosition() {
-  for (let slide of slides) {
+  for (const slide of slides) {
     slide.classList.remove('carousel__item--visible');
     slide.classList.add('carousel__item--hidden');
   }
