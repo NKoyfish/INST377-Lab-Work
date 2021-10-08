ad1d4d30e8211a5b10d2fe043853c656035e9065
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2021-10-07 22:24:41 -0400

    styling

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -21,18 +21,18 @@ async function windowActions() {
     console.log(event.value);
     const matchArray = findMatches(event.target.value, restaur);
     const html = matchArray
-      .map((search) => `<li>${search.name}<br>${search.category}
+      .map((search) => `<div class="button"><li style ="width:100%">${search.name}<br>${search.category}
       <br>${search.address_line_1}
       <br>${search.city}
-      <br>${search.zip}<br></li>`)
+      <br>${search.zip}<br></li></div>`)
       .join("");
     suggest.innerHTML = html;
   }
 
   searchInput.addEventListener("change", displayMatches);
   searchInput.addEventListener("keyup", (evt) => {
-    if (searchInput.value === "") {
-      suggest.innerHTML = "";
+    if (searchInput.value === "" || searchInput.value  === undefined) {
+      suggest.innerHTML = 'This is going to be something in two weeks and scrollable. <br><h1>Scrollable!!!</h1>';
     } else {
       displayMatches(evt);
       console.log(`looking up ${searchInput.innerHTML}`);
