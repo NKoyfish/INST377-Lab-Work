d908965f40feea3cc69ddb495846ff69663a1a23
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2021-10-08 21:08:39 -0400

    lab 7 more accurate changes

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,46 +1,78 @@
+let hashMap = new Map();
+var mymap = L.map("mapid").setView([38.974, -76.86609], 13);
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoibmtveWZpc2giLCJhIjoiY2t1aHRtZGgzMTcyOTJ2dDl1dDdpbjRtMiJ9.acTEbwBGCgecw_xcjJdubA",
+  }
+).addTo(mymap);
+
 async function windowActions() {
   const endpoint =
-  "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
-  const request = await fetch(endpoint)
+    "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+  const request = await fetch(endpoint);
   const restaur = await request.json();
   const searchInput = document.querySelector(".search");
   const suggest = document.querySelector(".xd");
-  const restCount = document.querySelector('.count');
-  let count = 0
-  console.log(restaur)
+  const restCount = document.querySelector(".count");
+  let count = 0;
+  let count2 = 0;
+  console.log(restaur);
   fetch(endpoint);
 
   function findMatches(wordToMatch, array) {
-    if (!wordToMatch) {
+    if (!wordToMatch || wordToMatch === "") {
       document.querySelector(".xd").innerHTML = "";
     }
     return restaur.filter((search) => {
       const regex = new RegExp(wordToMatch, "gi");
-      return search.name.match(regex) || search.category.match(regex) ||
-      search.address_line_1.match(regex) ||search.city.match(regex)||search.zip.match(regex) ;
+      return (
+        search.name.match(regex) ||
+        search.category.match(regex) ||
+        search.zip.match(regex)
+      );
     });
+  }
+  function hello() {
+    console.log(this.innerHTML, "stuff");
   }
   function displayMatches(event) {
     console.log(event.value);
     const matchArray = findMatches(event.target.value, restaur);
-    const html = matchArray
-      .map((search) => `<div class="button"><li style ="width:100%">${search.name}<br>${search.category}
-      <br>${search.address_line_1}
-      <br>${search.city}
-      <br>${search.zip}<br></li></div>`)
+    count2 = 0;
+    console.log('test',matchArray)
+    const html = matchArray.map(
+        (search) => `<li class="libutt"  style ="width:100%"> <span class="id${++count2}">
+        ${search.name}
+        <br>${search.category}
+            <br>${search.address_line_1}
+            <br>${search.city}
+            <br>${search.zip}<br>
+            </span></li>`
+      )
       .join("");
     suggest.innerHTML = html;
-    restCount.innerHTML = matchArray.length
+    restCount.innerHTML = matchArray.length;
+    //assigns each new list item a unique id
   }
- 
-  searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => {
-    if (searchInput.value === '' || searchInput.value  === undefined) {
-      restCount.innerHTML  = 0
-      suggest.innerHTML = 'This is going to be something in two weeks and scrollable. <br><h1>Scrollable!!!</h1>';
+
+  searchInput.addEventListener("change", displayMatches);
+  searchInput.addEventListener("keyup", (evt) => {
+    if (searchInput.value === '' || searchInput.value === undefined) {
+      restCount.innerHTML = 0;
+      suggest.innerHTML =
+        "This is going to be something in two weeks and scrollable. <br><h1>Scrollable!!!</h1>";
     } else {
       displayMatches(evt);
     }
   });
-}
+} //end of async
+
 window.onload = windowActions;
--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,46 +1,118 @@
+const fullyUnique = [];
+const layerG = [];
+
+mymap = undefined;
 async function windowActions() {
-  const endpoint =
-  "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
-  const request = await fetch(endpoint)
+  function mapInit() {
+    mymap = L.map('mapid').setView([38.974, -76.86609], 13);
+    L.tileLayer(
+      'https://api.maptiler.com/maps/toner/{z}/{x}/{y}.png?key=0im19NFqvOVkTZzwiWhj',
+      {
+        attribution:
+          '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          'pk.eyJ1IjoibmtveWZpc2giLCJhIjoiY2t1aHRtZGgzMTcyOTJ2dDl1dDdpbjRtMiJ9.acTEbwBGCgecw_xcjJdubA'
+      }
+    ).addTo(mymap);
+  }
+
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const request = await fetch(endpoint);
   const restaur = await request.json();
-  const searchInput = document.querySelector(".search");
-  const suggest = document.querySelector(".xd");
+  let pointMap = [];
+  const searchInput = document.querySelector('.search');
+  const suggest = document.querySelector('.xd');
   const restCount = document.querySelector('.count');
-  let count = 0
-  console.log(restaur)
+  const uniqueArray = [];
+  const count = 0;
+  console.log(restaur);
   fetch(endpoint);
 
   function findMatches(wordToMatch, array) {
     if (!wordToMatch) {
-      document.querySelector(".xd").innerHTML = "";
+      document.querySelector('.xd').innerHTML = '';
     }
     return restaur.filter((search) => {
-      const regex = new RegExp(wordToMatch, "gi");
-      return search.name.match(regex) || search.category.match(regex) ||
-      search.address_line_1.match(regex) ||search.city.match(regex)||search.zip.match(regex) ;
+      const regex = new RegExp(wordToMatch, 'gi');
+      return (
+        search.zip.match(regex)
+      );
     });
   }
   function displayMatches(event) {
-    console.log(event.value);
-    const matchArray = findMatches(event.target.value, restaur);
+    const matchArray1 = findMatches(event.target.value, restaur);
+    matchArray = matchArray1.slice(0, 5);
+    console.log('slice', matchArray);
     const html = matchArray
-      .map((search) => `<div class="button"><li style ="width:100%">${search.name}<br>${search.category}
+      .map(
+        (
+          search
+        ) => `<div class="button"><li style ="width:100%">${search.name}<br>${search.category}
       <br>${search.address_line_1}
       <br>${search.city}
-      <br>${search.zip}<br></li></div>`)
-      .join("");
+      <br>${search.zip}<br></li></div>`
+      )
+      .join('');
     suggest.innerHTML = html;
-    restCount.innerHTML = matchArray.length
-  }
+    restCount.innerHTML = matchArray.length;
+    const locationArr = [];
 
-  searchInput.addEventListener("change", displayMatches);
-  searchInput.addEventListener("keyup", (evt) => {
-    if (searchInput.value === "" || searchInput.value  === undefined) {
-      restCount.innerHTML  = 0
+    function getPoint(matchArrayElement) {
+      if (
+        matchArrayElement !== undefined
+        && matchArrayElement.geocoded_column_1 !== undefined
+        && matchArrayElement.geocoded_column_1.coordinates !== undefined
+      ) {
+        return [
+          matchArrayElement.geocoded_column_1.coordinates[1],
+          matchArrayElement.geocoded_column_1.coordinates[0]
+        ];
+      }
+      return undefined;
+    }
+    //
+    pointMap = matchArray.map((item) => getPoint(item));
+  }
+  function plotResults() {
+    let count = 0;
+    pointMap.forEach((ele) => {
+      if (count === 0) {
+        mymap.panTo(ele)
+        count = 2
+      }
+      if (ele !== undefined) {
+        if (!fullyUnique.includes(ele)) {
+          fullyUnique.push(ele);
+          L.marker(ele).addTo(mymap);
+        }
+      }
+    });
+    console.log(fullyUnique.length);
+  }
+  buttonClick = document.querySelector('.click');
+  buttonClick.addEventListener('click', plotResults);
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => {
+    if (searchInput.value === '' || searchInput.value === undefined) {
+      restCount.innerHTML = 0;
       suggest.innerHTML = 'This is going to be something in two weeks and scrollable. <br><h1>Scrollable!!!</h1>';
     } else {
       displayMatches(evt);
+      document.addEventListener('keyup', control);
+      function control(e) {
+        if (e.keyCode === 13) {
+          plotResults();
+        }
+      }
     }
   });
+  mapInit();
 }
 window.onload = windowActions;
+function getMarkers() {
+  console.log(fullyUnique);
+}
